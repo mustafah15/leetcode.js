129c87e380240eb23465e45024082e00670b9f5a
mustafa <mustafa.hussain93@gmail.com>
2020-01-21 00:45:17 +0200

    add dfs folder

---
+++ DepthFirstSearch/MinimumDepthOfBinaryTree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if(root == null) return 0;
+    
+    if(root.left == null && root.right == null) return 1;
+    if(root.left == null) return minDepth(root.right) + 1;
+    if(root.right == null) return minDepth(root.left) + 1;
+
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+};
