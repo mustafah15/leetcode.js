eb24934220a285c35ff132496f09e5738c00dbd1
mustafa <mustafa.hussain93@gmail.com>
2020-01-20 15:01:34 +0200

    add 199 & 107

---
+++ BreadthFirstSearch/BinaryTreeRightSideView.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    let node = root;
+    const queue= [];
+    const visited = [];
+    if(node === null) return visited;
+    queue.push(node);
+    while(queue.length > 0) {
+        let level = [];
+        let num = queue.length;
+        for(let i=0; i < num; i++) {
+            node = queue.shift();
+            level.push(node.val);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        visited.push(level.pop());
+    }
+    return visited;
+
+};
