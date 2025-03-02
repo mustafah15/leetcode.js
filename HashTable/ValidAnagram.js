b49e07d1ea7a5f5619d1e857ad3f869700856d09
Mustafa Hussain <mustafa.hussain93@gmail.com>
2025-03-02 20:55:00 +0100

    update

--- HashTable/ValidAnagram.js
+++ HashTable/ValidAnagram.js
@@ -5,30 +5,15 @@
  */
 
 var isAnagram = function(s, t) {
-  const arr_s = new Map();
-  const arr_t = new Map();
-  if(s.length !== t.length) {
+  if (s.length !== t.length) {
     return false;
   }
 
+  const count = new Array(26).fill(0);
+
   for (let i = 0; i < s.length; i++) {
-    arr_s.set(s[i], arr_s.get(s[i]) ? arr_s.get(s[i]) + 1 : 1 );
+      count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+      count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
   }
-
-  for (let i = 0; i < t.length; i++) {
-    arr_t.set(t[i], arr_t.get(t[i]) ? arr_t.get(t[i]) + 1 : 1  );
-  }
-
-
-  for(let i = 0; i < s.length; i++)
-  {
-    let value_s = arr_s.get(s[i]);
-    let value_t = arr_t.get(s[i]);      
-
-    if(value_s !== value_t) {
-      return false;
-    }
-  }
-
-  return true;
+  return count.every(val => val === 0);
 };
